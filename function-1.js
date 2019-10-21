d125befded8d5e4d04c8e33a389b81ae0a57e106
grobelDev <grobeldev@gmail.com>
2019-10-21 11:56:38 -0700

    step 8

--- function-1.js
+++ function-1.js
@@ -2,12 +2,20 @@ function yearOfBirth(age) {
     if(age < 0) {
         throw new Error("Age cannot be negative");
     }
+
     return 2019 - age;
 }
 
 function createGreeting(name, age) {
+    if(typeof name !== 'string') {
+        throw new Error("Name is undefined or not a string");   
+    }
+    if(typeof age !== 'number') {
+        throw new Error("Age is undefined or not a number");   
+    }
+
     return `Hi, my name is ${name} and I \'m ${age} years old on ${yearOfBirth(age)}`;
 }
 
-createGreeting("Harrison", 1);
-
+let testGreet = createGreeting('Harrison', 123);
+console.log(testGreet);
