8618ac837425b30e900c2ab07926b923341787fc
AnthonyTB <Anthonylol@protonmail.com>
2019-10-21 12:23:16 -0700

    Step 2

--- function-2.js
+++ function-2.js
@@ -1,5 +1,21 @@
+'use strict';
+
 function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
 console.log(jediName('Anthony', 'Bostic'));
+
+function beyond(num) {
+  if (num === Infinity) {
+    console.log('And beyond');
+  } else if (num !== Infinity && num > 0) {
+    console.log('To infinity');
+  } else if (num !== Infinity && num < 0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  }
+}
+
+beyond(6 / 0);
