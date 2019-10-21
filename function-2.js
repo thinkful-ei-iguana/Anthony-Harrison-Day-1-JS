dc8fec1a0f2708be13b22ecb6890c9b95c228715
AnthonyTB <Anthonylol@protonmail.com>
2019-10-21 12:40:04 -0700

    Step 3

--- function-2.js
+++ function-2.js
@@ -19,3 +19,22 @@ function beyond(num) {
 }
 
 beyond(6 / 0);
+
+function decode(word) {
+  let newArr = word.split('');
+  for (let i of newArr) {
+    if (i === 'a') {
+      return newArr[1];
+    } else if (i === 'b') {
+      return newArr[2];
+    } else if (i === 'c') {
+      return newArr[3];
+    } else if (i === 'd') {
+      return newArr[4];
+    } else {
+      return ' ';
+    }
+  }
+}
+
+console.log(decode('droop'));
