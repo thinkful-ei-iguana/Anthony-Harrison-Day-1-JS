077f585b29d259dfe57ae30a7ce11e0a14e2dca3
AnthonyTB <Anthonylol@protonmail.com>
2019-10-21 21:32:39 -0700

    Fixed reduce problem

--- higher.js
+++ higher.js
@@ -1,79 +1,77 @@
+'use strict';
+
 function repeat(fn, n) {
-    for(i = 0; i < n; i++) {
-        fn();
-    }
+  for (i = 0; i < n; i++) {
+    fn();
+  }
 }
 
 function hello() {
-    console.log("Hello World");
+  console.log('Hello World');
 }
 
 function goodbye() {
-    console.log("Goodbye World");
+  console.log('Goodbye World');
 }
 
 // repeat(goodbye, 5);
 
 function filter(arr, fn) {
-    // TASK: Define your function here
-    let newArr = [];
+  // TASK: Define your function here
+  let newArr = [];
 
-    for(i = 0; i < arr.length; i++) {
-        for(j = 0; j < arr[i].length; j++) {
-            if(arr[i][j] === "R") {
-                newArr.push(arr[i]);
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === 'R') {
+        newArr.push(arr[i]);
+      }
     }
-    return newArr;
+  }
+  return newArr;
 }
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
 // First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const myNames = [ 'Rich', 'Joe', 'Bhaumik', 'Ray' ];
 
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
+  // This is known as a "predicate function" - it's a function that
+  // only returns a boolean
+  return name[0] === 'R';
 });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 function hazardWarningCreator(typeOfWarning) {
-    let warningCounter = 0;
+  let warningCounter = 0;
 
-    return function(location) {
-        warningCounter++;
-        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-    }
+  return function(location) {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
 }
 
 // const rocksWarning = hazardWarningCreator('Rocks on the Road');
 // rocksWarning('Main St and Pacific Ave');
 
-let turtleArray = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+let turtleArray = [ [ 0, 0 ], [ 0, 5 ], [ -1, -3 ], [ -3, 1 ], [ 2, -4 ], [ 3, 2 ] ];
 // let newArray = [];
 
-const result = turtleArray.filter(coordinates => {
-
-    if(coordinates[0] >= 0 && coordinates[1] >= 0) {
-        return coordinates;
-    }
-
+const result = turtleArray.filter((coordinates) => {
+  if (coordinates[0] >= 0 && coordinates[1] >= 0) {
+    return coordinates;
+  }
 });
 
-const result_2 = turtleArray.map(coordinates => {
-
-    // if(coordinates[0] >= 0 && coordinates[1] >= 0) {
-    //     return coordinates;
-    // }
-    return coordinates[0] + coordinates[1];
-
+const result_2 = turtleArray.map((coordinates) => {
+  // if(coordinates[0] >= 0 && coordinates[1] >= 0) {
+  //     return coordinates;
+  // }
+  return coordinates[0] + coordinates[1];
 });
 
 let counter = 1;
@@ -89,22 +87,18 @@ let counter = 1;
 
 let decodeString = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 // NO_NE
-let decodeArray = decodeString.split(' ').reduce(myFunc);
+let decodeArray = decodeString.split(' ').reduce(myFunc, '');
 
 let acc = '';
 
 function myFunc(param1, param2) {
-    if(param1 === 'noggin' && param2 === 'oreo') {
-        return 'NO';
-    }
+  if (param2.length === 3) {
+    return param1 + ' ';
+  } else {
+    return param1 + param2.slice(-1).toUpperCase();
+  }
 
-    if(param2.length === 3) {
-        return param1 + ' ';
-    } else {
-        return param1 + param2.slice(-1).toUpperCase();
-    }
-
-    // return param1 + param2;
+  // return param1 + param2;
 }
 console.log(decodeArray);
 
